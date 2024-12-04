e42902ee4793af0f840e148d430eb5de7dcbc67f
WallaceVidoto <wallacevidoto.tech@gmail.com>
2024-12-03 22:20:51 -0300

    updates

--- src/app/components/pages/delivery/delivery.component.ts
+++ src/app/components/pages/delivery/delivery.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonCustomComponent } from '../../inputs/button-custom/button-custom.component';
+import { InputButtonGenericComponent } from "../../inputs/input-button-generic/input-button-generic.component";
 
 @Component({
   selector: 'app-delivery',
   standalone: true,
-  imports: [],
+  imports: [ButtonCustomComponent, InputButtonGenericComponent],
   templateUrl: './delivery.component.html',
   styleUrl: './delivery.component.scss'
 })
 export class DeliveryComponent {
+ButtonSVG: any;
 
 }
